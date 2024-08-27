172862a4bf7b332cc6d60d48c4bad172c4a46198
Lakshan Tharaka <tharakahitibandara@gmail.com>
2024-08-27 20:29:44 +0530

    improve the scanner code

--- src/services/dataScanner.js
+++ src/services/dataScanner.js
@@ -1,10 +1,11 @@
 const logger = require('../config/logger');
 const makeCall = require('../services/makeCalls');
 
-
-
 // List of words to check in the subject
-const wordList = ['Zabbix agent is not available', 'High CPU utilization (over 90% for 1m)'];
+const wordList = ['Zabbix agent is not available', 'High CPU utilization (over 90% for 1m)','High memory utilization (>90% for 5m)'];
+
+// List of words to omit from making a call
+const omitList = ['Resolved'];
 
 /**
  * Function to check if the subject contains any words from the list.
@@ -12,19 +13,23 @@ const wordList = ['Zabbix agent is not available', 'High CPU utilization (over 9
  * @returns {number} - Returns 1 if a match is found, otherwise returns 0.
  */
 const dataScanner = (subject) => {
+    // Check if the subject contains any words from the omit list
+    for (const omitWord of omitList) {
+        if (subject.includes(omitWord)) {
+            logger.info(`Omit word found: ${omitWord} in subject: ${subject}`);
+            return 0;
+        }
+    }
+
     // Iterate over the word list and check if the subject contains any of the words
     for (const word of wordList) {
         if (subject.includes(word)) {
             logger.info(`Match found: ${word} in subject: ${subject}`);
             makeCall(+94702294400);
             return 1;
-            //I need to make a api call to the external service
-            // makeCall('+94702294400');
-            
-            
-
         }
     }
+
     logger.info(`No match found in subject: ${subject}`);
     return 0;
 };
